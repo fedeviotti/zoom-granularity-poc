cfebd84dea3f004a536286a7a8a31d1223d23314
Federico Viotti <federico.viotti@nhoa.energy>
2024-01-26 15:31:32 +0100

    feat: change granularity on sync charts while zooming (wip)

--- src/LineChart.tsx
+++ src/LineChart.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Chart from 'react-apexcharts';
-import {DataSeries, dataSeries, dataSeries2} from "./dataSeries.ts";
+import {DataSeries, dataSeries} from "./dataSeries.ts";
 import {
   MS_IN_ONE_MONTH,
-  OPTIONS, OPTIONS2
+  OPTIONS
 } from "./constants.ts";
 import {ApexOptions} from "apexcharts";
 import {Flex, Progress} from "@chakra-ui/react";
@@ -48,10 +48,8 @@ const getTwoDaysDataSeries = ({ data }: GetSeriesProps) => {
 }
 
 export const LineChart = () => {
-  const [series, setSeries] = React.useState<Series[]>([]);
-  const [series2, setSeries2] = React.useState<Series[]>([]);
+  const [series, setSeries] = React.useState<Series[][]>([]);
   const [isFetchingData, setIsFetchingData] = React.useState<boolean>(false);
-  const [isFetchingData2, setIsFetchingData2] = React.useState<boolean>(false);
   const granularity = React.useRef<"ONE_DAY" | "TWO_DAYS">("TWO_DAYS");
   const zoomLimits = React.useRef<{min: number, max: number}>({min: 0, max: 0});
 
@@ -59,148 +57,86 @@ export const LineChart = () => {
     if (series.length > 0) {
       return;
     }
-
-    const data = getTwoDaysDataSeries({data: dataSeries});
-    setSeries([{ data, name: "NHOA" }]);
+    const tmpSeries: Series[][] = [];
+    dataSeries.forEach((series, i) => {
+      const data = getTwoDaysDataSeries({data: series});
+      tmpSeries.push([{ data, name: `NHOA ${i}` }]);
+    });
+    setSeries(tmpSeries);
   }, [series]);
-
-  React.useEffect(() => {
-    if (series2.length > 0) {
-      return;
-    }
-
-    const data = getTwoDaysDataSeries({data: dataSeries2});
-    setSeries2([{ data, name: "NHOA 2" }]);
-  }, [series2]);
 
   const beforeDataFetch = () => {
     setIsFetchingData(true);
-    ApexCharts.exec('zoom-chart', 'updateOptions',
-      {
-        chart: {
-          zoom: { enabled: false },
-          toolbar: { show: false }
-        }
-      });
+    OPTIONS.forEach((_option, i) => {
+      ApexCharts.exec(`zoom-chart-${i}`, 'updateOptions',
+        {
+          chart: {
+            zoom: { enabled: false },
+            toolbar: { show: false }
+          }
+        });
+    });
   }
 
   const dataFetch = ({data, xMin, xMax, targetGranularity}: DataFetchProps) => {
-    ApexCharts.exec('zoom-chart', 'updateSeries', [{ data }]);
-    if (xMin !== 0 && xMax !== 0) {
-      ApexCharts.exec('zoom-chart', 'zoomX', xMin, xMax);
-    }
+    OPTIONS.forEach((_option, i) => {
+      ApexCharts.exec(`zoom-chart-${i}`, 'updateSeries', [{ data }]);
+      if (xMin !== 0 && xMax !== 0) {
+        ApexCharts.exec(`zoom-chart-${i}`, 'zoomX', xMin, xMax);
+      }
+    });
     afterDataFetch(targetGranularity);
   }
 
   const afterDataFetch = (targetGranularity: Granularity) => {
     setIsFetchingData(false);
-    ApexCharts.exec('zoom-chart', 'updateOptions',
-      {
-        chart: {
-          zoom: { enabled: true },
-          toolbar: { show: true }
-        }
-      });
-    granularity.current = targetGranularity;
-  }
-
-  const beforeDataFetch2 = () => {
-    setIsFetchingData2(true);
-    ApexCharts.exec('zoom-chart-2', 'updateOptions',
-      {
-        chart: {
-          zoom: { enabled: false },
-          toolbar: { show: false }
-        }
-      });
-  }
-
-  const dataFetch2 = ({data, xMin, xMax, targetGranularity}: DataFetchProps) => {
-    ApexCharts.exec('zoom-chart-2', 'updateSeries', [{ data }]);
-    if (xMin !== 0 && xMax !== 0) {
-      ApexCharts.exec('zoom-chart-2', 'zoomX', xMin, xMax);
-    }
-    afterDataFetch2(targetGranularity);
-  }
-
-  const afterDataFetch2 = (targetGranularity: Granularity) => {
-    setIsFetchingData2(false);
-    ApexCharts.exec('zoom-chart-2', 'updateOptions',
-      {
-        chart: {
-          zoom: { enabled: true },
-          toolbar: { show: true }
-        }
-      });
+    OPTIONS.forEach((_option, i) => {
+      ApexCharts.exec(`zoom-chart-${i}`, 'updateOptions',
+        {
+          chart: {
+            zoom: { enabled: true },
+            toolbar: { show: true }
+          }
+        });
+    });
     granularity.current = targetGranularity;
   }
 
   const beforeZoom = (_chartContext: never, options: ApexOptions) => {
-    const xMin = options?.xaxis?.min || 0;
-    const xMax = options?.xaxis?.max || 0;
-    const zoomDiff = xMax - xMin;
+    beforeDataFetch();
+    OPTIONS.forEach((_option, i) => {
+      const xMin = options?.xaxis?.min || 0;
+      const xMax = options?.xaxis?.max || 0;
+      const zoomDiff = xMax - xMin;
 
-    if (zoomLimits.current.min !== xMin && zoomLimits.current.max !== xMax) {
-      zoomLimits.current = { min: xMin, max: xMax };
+      if (zoomLimits.current.min !== xMin && zoomLimits.current.max !== xMax) {
+        zoomLimits.current = { min: xMin, max: xMax };
 
-      if (zoomDiff < MS_IN_ONE_MONTH && granularity.current === "TWO_DAYS") {
-        beforeDataFetch()
-        setTimeout(() => {
-          console.log("increase granularity");
-          dataFetch({data: getOneDayDataSeries({data: dataSeries}), xMin, xMax, targetGranularity: "ONE_DAY"});
-        }, 1500);
-      } else if (zoomDiff > MS_IN_ONE_MONTH && granularity.current === "ONE_DAY") {
-        beforeDataFetch();
-        setTimeout(() => {
-          console.log("decrease granularity");
-          dataFetch({data: getTwoDaysDataSeries({data: dataSeries}), xMin, xMax, targetGranularity: "TWO_DAYS"});
-        }, 1500);
+        if (zoomDiff < MS_IN_ONE_MONTH && granularity.current === "TWO_DAYS") {
+          setTimeout(() => {
+            console.log("increase granularity");
+            dataFetch({data: getOneDayDataSeries({data: dataSeries[i]}), xMin, xMax, targetGranularity: "ONE_DAY"});
+          }, 1500);
+        } else if (zoomDiff > MS_IN_ONE_MONTH && granularity.current === "ONE_DAY") {
+          setTimeout(() => {
+            console.log("decrease granularity");
+            dataFetch({data: getTwoDaysDataSeries({data: dataSeries[i]}), xMin, xMax, targetGranularity: "TWO_DAYS"});
+          }, 1500);
+        }
+      } else {
+        zoomLimits.current = { min: 0, max: 0 };
       }
-    } else {
-      zoomLimits.current = { min: 0, max: 0 };
-    }
+    });
   }
 
   const beforeResetZoom = () => {
     beforeDataFetch();
-    setTimeout(() => {
-      console.log("reset granularity");
-      dataFetch({data: getTwoDaysDataSeries({data: dataSeries}), xMin: 0, xMax: 0, targetGranularity: "TWO_DAYS"});
-    }, 1500);
-  }
-
-  const beforeZoom2 = (_chartContext: never, options: ApexOptions) => {
-    const xMin = options?.xaxis?.min || 0;
-    const xMax = options?.xaxis?.max || 0;
-    const zoomDiff = xMax - xMin;
-
-    if (zoomLimits.current.min !== xMin && zoomLimits.current.max !== xMax) {
-      zoomLimits.current = { min: xMin, max: xMax };
-
-      if (zoomDiff < MS_IN_ONE_MONTH && granularity.current === "TWO_DAYS") {
-        beforeDataFetch2()
-        setTimeout(() => {
-          console.log("increase granularity");
-          dataFetch2({data: getOneDayDataSeries({data: dataSeries2}), xMin, xMax, targetGranularity: "ONE_DAY"});
-        }, 1500);
-      } else if (zoomDiff > MS_IN_ONE_MONTH && granularity.current === "ONE_DAY") {
-        beforeDataFetch2();
-        setTimeout(() => {
-          console.log("decrease granularity");
-          dataFetch2({data: getTwoDaysDataSeries({data: dataSeries2}), xMin, xMax, targetGranularity: "TWO_DAYS"});
-        }, 1500);
-      }
-    } else {
-      zoomLimits.current = { min: 0, max: 0 };
-    }
-  }
-
-  const beforeResetZoom2 = () => {
-    beforeDataFetch2();
-    setTimeout(() => {
-      console.log("reset granularity");
-      dataFetch2({data: getTwoDaysDataSeries({data: dataSeries2}), xMin: 0, xMax: 0, targetGranularity: "TWO_DAYS"});
-    }, 1500);
+    OPTIONS.forEach((_option, i) => {
+      setTimeout(() => {
+        console.log("reset granularity");
+        dataFetch({data: getTwoDaysDataSeries({data: dataSeries[i]}), xMin: 0, xMax: 0, targetGranularity: "TWO_DAYS"});
+      }, 1500);
+    });
   }
 
   return (
@@ -208,15 +144,15 @@ export const LineChart = () => {
       <Flex h="2xl" direction="column" gap="8" justify="end">
         {isFetchingData && <Progress size='xs' isIndeterminate w={800} />}
         <Chart
-          options={{...OPTIONS, chart: { ...OPTIONS.chart, events: { beforeZoom, beforeResetZoom } }}}
-          series={series} type="area" width={800}
+          options={{...OPTIONS[0], chart: { ...OPTIONS[0].chart, events: { beforeZoom, beforeResetZoom } }}}
+          series={series[0] || []} type="area" width={800}
         />
       </Flex>
       <Flex h="2xl" direction="column" gap="8" justify="end">
-        {isFetchingData2 && <Progress size='xs' isIndeterminate w={800} />}
+        {isFetchingData && <Progress size='xs' isIndeterminate w={800} />}
         <Chart
-          options={{...OPTIONS2, chart: { ...OPTIONS2.chart, events: { beforeZoom: beforeZoom2, beforeResetZoom: beforeResetZoom2 } }}}
-          series={series} type="area" width={800}
+          options={{...OPTIONS[1], chart: { ...OPTIONS[1].chart, events: { beforeZoom, beforeResetZoom } }}}
+          series={series[1] || []} type="area" width={800}
         />
       </Flex>
     </Flex>
--- src/constants.ts
+++ src/constants.ts
@@ -2,9 +2,9 @@ import {ApexOptions} from "apexcharts";
 
 export const MS_IN_ONE_MONTH = 2592000000;
 
-export const OPTIONS: ApexOptions = {
+export const OPTIONS: ApexOptions[] = [{
   chart: {
-    id: "zoom-chart",
+    id: "zoom-chart-0",
     group: "nhoa-chart-group",
     animations: {
       enabled: false
@@ -62,66 +62,65 @@ export const OPTIONS: ApexOptions = {
       }
     }
   },
-};
-
-export const OPTIONS2: ApexOptions = {
-  chart: {
-    id: "zoom-chart-2",
-    group: "nhoa-chart-group",
-    animations: {
+},
+  {
+    chart: {
+      id: "zoom-chart-1",
+      group: "nhoa-chart-group",
+      animations: {
+        enabled: false
+      },
+      type: "area",
+      stacked: false,
+      zoom: {
+        type: "x",
+        enabled: true,
+        autoScaleYaxis: true,
+      },
+      toolbar: {
+        autoSelected: "zoom"
+      },
+    },
+    dataLabels: {
       enabled: false
     },
-    type: "area",
-    stacked: false,
-    zoom: {
-      type: "x",
-      enabled: true,
-      autoScaleYaxis: true,
-    },
-    toolbar: {
-      autoSelected: "zoom"
-    },
-  },
-  dataLabels: {
-    enabled: false
-  },
-  markers: {
-    size: 0
-  },
-  title: {
-    text: "Stock Price Movement",
-    align: "left"
-  },
-  fill: {
-    type: "gradient",
-    gradient: {
-      shadeIntensity: 1,
-      inverseColors: false,
-      opacityFrom: 0.5,
-      opacityTo: 0,
-      stops: [0, 90, 100]
-    }
-  },
-  yaxis: {
-    labels: {
-      minWidth: 40,
-      formatter: function (val: number) {
-        return (val / 1000000).toFixed(0);
-      }
+    markers: {
+      size: 0
     },
     title: {
-      text: "Price"
-    }
-  },
-  xaxis: {
-    type: "datetime"
-  },
-  tooltip: {
-    shared: false,
-    y: {
-      formatter: function (val: number) {
-        return (val / 1000000).toFixed(0);
+      text: "Stock Price Movement",
+      align: "left"
+    },
+    fill: {
+      type: "gradient",
+      gradient: {
+        shadeIntensity: 1,
+        inverseColors: false,
+        opacityFrom: 0.5,
+        opacityTo: 0,
+        stops: [0, 90, 100]
       }
-    }
-  },
-};
+    },
+    yaxis: {
+      labels: {
+        minWidth: 40,
+        formatter: function (val: number) {
+          return (val / 1000000).toFixed(0);
+        }
+      },
+      title: {
+        text: "Price"
+      }
+    },
+    xaxis: {
+      type: "datetime"
+    },
+    tooltip: {
+      shared: false,
+      y: {
+        formatter: function (val: number) {
+          return (val / 1000000).toFixed(0);
+        }
+      }
+    },
+  }];
